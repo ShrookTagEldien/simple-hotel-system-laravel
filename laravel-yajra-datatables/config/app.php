36ab93b7b1857bb59a199b7dff3403bc61397a7a
Fatma <fatma.tarek204@gmail.com>
2021-03-28 03:10:26 +0200

    error in yjra

--- laravel-yajra-datatables/config/app.php
+++ laravel-yajra-datatables/config/app.php
@@ -161,12 +161,11 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        //Yajra\DataTables\DataTablesServiceProvider::class,
-        //yajra\Datatables\DatatablesServiceProvider
+
         /*
          * Package Service Providers...
          */
-       
+
         /*
          * Application Service Providers...
          */
@@ -175,10 +174,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        
-        //Yajra\DataTables\DataTablesServiceProvider::class,
-       // Yajra\DataTables\DataTablesServiceProvider::class,
-        Yajra\Datatables\DatatablesServiceProvider::class,
 
     ],
 
@@ -232,10 +227,6 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        
-        //'DataTables' => Yajra\DataTables\Facades\DataTables::class,
-        //'Datatables' => Yajra\Datatables\Facades\Datatables::class,
-        'DataTables' => Yajra\DataTables\Facades\DataTables::class,
 
     ],
 
--- laravel-yajra-datatables/routes/web.php
+++ laravel-yajra-datatables/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,9 +16,3 @@ use Illuminate\Support\Facades\Auth;
 Route::get('/', function () {
     return view('welcome');
 });
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::get('students', [StudentController::class, 'index']);
-Route::get('students/list', [StudentController::class, 'getStudents'])->name('students.list');
